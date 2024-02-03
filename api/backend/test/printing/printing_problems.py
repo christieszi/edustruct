abc1505c36cdb05cdfcc820d7b0c05b963b1331b
Krish Maha <krish@mahas.net>
2024-02-03 18:13:01 +0000

    Set up dummy printing problems with block placeholders

---
+++ api/backend/test/printing/printing_problems.py
@@ -0,0 +1,27 @@
+# print strings
+user_problem1 = """
+def print_hello_world():
+    block
+"""
+
+user_problem2 = """
+def print_name():
+    name = "John"
+    block
+    name = "Adam"
+    block
+"""
+
+# print arguments
+user_problem3 = """
+def print_name_age(name, age):
+    block
+    block
+"""
+
+# concatenation
+# print("name: " + name + " surname: " + surname)
+user_problem4 = """
+def print_name_age(firstname, surname):
+    block
+"""
