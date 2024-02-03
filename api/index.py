9ae34d71d7fcd990388ae4786791b2639b25fa8d
christieszi <kristi060802@gmail.com>
2024-02-03 17:52:59 +0000

    Add the button

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,3 +17,14 @@ def excercise():
 @app.route('/game')
 def game():
     return render_template('index.html')
+
+@app.route('/visual')
+def visual():
+    return render_template('visualisation.html', printed_text="")
+
+@app.route('/print_text', methods=['POST'])
+def print_text():
+    button_text = request.form['button_text']
+    current_text = request.form['current_text']
+    printed_texts = current_text + " new line print(" + button_text + ")"
+    return render_template('visualisation.html', printed_text=printed_texts)
