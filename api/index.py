b16aef1d5c3f8714a4c01608fb32b7828e876051
Esh <32418603+Esh07@users.noreply.github.com>
2024-02-03 20:57:37 +0000

    chore: Update the Flask App name with our name

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
+# replace the name of the app with the name of our app
+app.config['APP_NAME'] = 'EduStruct'
 
 @app.route('/')
 def home():
