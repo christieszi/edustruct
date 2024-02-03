e03c5fa0227e33965cc29318e92ce5c1147bdba5
christieszi <kristi060802@gmail.com>
2024-02-03 20:00:19 +0000

    Add process code

--- api/index.py
+++ api/index.py
@@ -30,6 +30,14 @@ def process():
     data = request.get_json() # retrieve the data sent from JavaScript
     # process the data using Python code
     value = data['value']
-    last.append("print(" + value + ")<br />")
-    result = ''.join(str(x) for x in last)
+    last.append("print(" + value + ")")
+    result = '<br />'.join(str(x) for x in last)
+    return jsonify(result=result) # return the result to JavaScript
+
+@app.route('/process_code', methods=['POST'])
+def process_code():
+    global last
+    code = "\n".join(last)
+    print("Code = " + code)
+    result = eval(code)
     return jsonify(result=result) # return the result to JavaScript
