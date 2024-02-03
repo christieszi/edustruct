ded38f4c3d2c14c7e19db3a2ac983c09933f8e25
Dylan Lapeyre <ecl21@ic.ac.uk>
2024-02-03 20:40:55 +0000

    First mockup of home and exercise pages

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
@@ -10,6 +10,6 @@ def home():
 def about():
     return 'About'
 
-@app.route('/excercise')
-def excercise():
-    return render_template('code.html')
+@app.route('/exercises')
+def excercises():
+    return render_template('exercises.html')
