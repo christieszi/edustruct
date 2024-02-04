98e38c353306e4883ba18fff154dd22a0458b918
christieszi <kristi060802@gmail.com>
2024-02-04 05:16:17 +0000

    Fix monkey movement in the grid

--- api/index.py
+++ api/index.py
@@ -21,7 +21,7 @@ def excercise2():
 
 @app.route('/3')
 def excercise3():
-    return render_template('code.html', print_button=False, assign_vars = False, list_button = True)
+    return render_template('ex3.html', print_button=False, assign_vars = False, list_button = True)
 
 last = []
 
@@ -35,14 +35,17 @@ def process_print():
     result = '<br />'.join(str(x) for x in last)
     return jsonify(result=result) # return the result to JavaScript
 
+last_scaled = []
 @app.route('/process_assign', methods=['POST'])
 def process_assign():
     global last
+    global last_scaled
     data = request.get_json() # retrieve the data sent from JavaScript
     # process the data using Python code
     value = data['value']
     var_name = data['var']
     last.append(var_name + " = " + value)
+    last_scaled.append(var_name + " = 100*(" + value+ ")")
     result = '<br />'.join(str(x) for x in last)
     return jsonify(result=result) # return the result to JavaScript
 
@@ -57,8 +60,20 @@ def process_list_assign():
     result = '<br />'.join(str(x) for x in last)
     return jsonify(result=result) # return the result to JavaScript
 
+@app.route('/process_list_access', methods=['POST'])
+def process_list_access():
+    global last
+    data = request.get_json() # retrieve the data sent from JavaScript
+    # process the data using Python code
+    listName = data['listName']
+    var_name = data['var']
+    index = data['index']
+    last.append(var_name + " = " + listName + "[" + index + "]")
+    result = '<br />'.join(str(x) for x in last)
+    return jsonify(result=result) # return the result to JavaScript
+
 x = 0
-y = 0
+y = 4
 
 @app.route('/process_code', methods=['POST'])
 def process_code():
@@ -68,7 +83,7 @@ def process_code():
     last_editted = []
     last_editted.insert(0, "x=" + str(x))
     last_editted.insert(0, "y=" + str(y))
-    for elem in last:
+    for elem in last_scaled:
         last_editted.append(elem)
     code = "\n".join(last_editted)
     old_stdout = sys.stdout
@@ -84,7 +99,7 @@ def process_code():
         result = "Syntax Error"
 
     x = loc['x']
-    y = loc['y']
+    y = 400 - loc['y']
     print(result)
     return jsonify(result = result, result_x=x, result_y=y) # return the result to JavaScript
 
